08d3d89b840168fd38a7f8ee6931772308fa7b7f
DarkWhige <clement.duboille@epitech.eu>
2024-06-03 18:24:28 +0200

    Add comments widgetInfo.hpp

--- widgetInfo.hpp
+++ widgetInfo.hpp
@@ -5,27 +5,40 @@
 
 namespace Window
 {
+    /// @brief toutes les datas qu'un widget peut avoir
     struct WidgetInfo {
-        std::string type;
-        std::string mouse_type;
-        std::string mouse_path;
+        // --------------------------------------------------------- général ---------------------------------------------------- //
 
-        bool button;
-        std::string action;
+        std::string type;   // type de widget (A PASSER EN ENUM ASSEZ RAPIDEMENT)
+        sf::Vector2f pos;   // position du widget
+        sf::Vector2f size;  // taille du widget
 
-        sf::Vector2f pos;
-        sf::Vector2f size;
+        // --------------------------------------------------------- souris ----------------------------------------------------- //
 
-        sf::Color color;
-        sf::Color outline_color;
-        sf::Color back_color;
-        float outline_thickness;
+        std::string mouse_type; // type de curseur de souris attribuer (A PASSER EN ENUM ASSEZ RAPIDEMENT)
 
-        std::string path;
+        // --------------------------------------------------------- bouton ----------------------------------------------------- //
 
-        std::string content;
-        std::string font_type;
-        std::string font_shape;
-        int font_size;
+        bool button;    // est-ce un bouton ou non ?
+        std::string action; // quelle action peut provoquer le bouton ?
+
+        // --------------------------------------------------- couleurs / forme ------------------------------------------------- //
+
+        sf::Color color;    // couleur de l'élément du widget (ex : couleur du texte)
+        sf::Color outline_color;    // couleur de la bordure
+        sf::Color back_color;       // couleur de l'arrière du widget
+        float outline_thickness;    // épaisseur de la bordure
+
+        // --------------------------------------------------------- police ----------------------------------------------------- //
+
+        std::string content;    // contenu du widget (ex : contenu du texte)
+        std::string font_type;  // type de police (ex : arial)
+        std::string font_shape; // forme de la police (ex : italique; normale, etc...)
+        int font_size;  // taille de la police
+
+        // --------------------------------------------------------- autre ------------------------------------------------------ //
+
+        std::string path;   // chemin vers un élément demandé (ex : chemin vers l'image ou la font d'un texte)
+        bool can_move;  // est-ce que le widget peut etre bougé par l'utilisateur
     };
 }
