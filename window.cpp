78260a635c87fd580bb7c941d865f1be0ba0cbc6
DarkWhige <clement.duboille@epitech.eu>
2024-06-03 17:44:13 +0200

    Add comments and Fix window constructor

--- window.cpp
+++ window.cpp
@@ -5,45 +5,45 @@ namespace Window
 {
     my_Window::~my_Window()
     {
-        widgets.clear();
-        win.reset();
+        widgets.clear();    // suppression de la liste de widget
+        win.reset();    // suppréssion de la window
     }
 
-    void my_Window::window(int h, int l, std::string title)
+    void my_Window::window(my_Window::Window_info data)
     {
-        win = std::make_unique<sf::RenderWindow>(sf::VideoMode(l, h), title);
-        win->clear(sf::Color::White);
-        win->setFramerateLimit(24);
+        win = std::make_unique<sf::RenderWindow>(sf::VideoMode(data.height, data.lenght), data.title);   // création de la fenètre avec les dimension et le titre demandé
+        win->clear(data.bg_color);   // setup le fond de la fenètre en blanc
+        win->setFramerateLimit(data.FPS);     // setup la limite d'image par seconde de la fenètre au montant demandé
         win->display();
     }
 
     void my_Window::loop()
     {
-        while (win->isOpen()) {
-            event_handler();
-            win->clear(sf::Color::White);
-            for (size_t i = 0; i < widgets.size(); ++i) {
-                widgets[i]->draw(*win);
+        while (win->isOpen()) { // tant que la window est ouverte
+            event_handler();    // fonction de gestion des évenements
+            win->clear(sf::Color::White);  // actualisation du background
+            for (size_t i = 0; i < widgets.size(); ++i) {   // iteration dans la liste des widgets
+                widgets[i]->draw(*win); // affichage des widgets
             }
-            win->display();
+            win->display(); // affichage de l'enssemble de la window
         }
-    }
-
-    void my_Window::addWidget(const WidgetInfo& data)
-    {
-        std::shared_ptr<my_widget> newWidget = std::make_shared<my_widget>();
-        newWidget->set_widget(data, win.get());
-        widgets.push_back(newWidget);
-    }
-
-    void my_Window::delWidget()
-    {
-        widgets.clear();
-        win->clear();
     }
 
     sf::RenderWindow* my_Window::get_win() const
     {
-        return win.get();
+        return win.get();   // recupération de la window
+    }
+
+    void my_Window::addWidget(const WidgetInfo& data)
+    {
+        std::shared_ptr<my_widget> newWidget = std::make_shared<my_widget>();   // creation d'un nouveau pointeur vers un widget
+        newWidget->set_widget(data, win.get()); // set le pointeur
+        widgets.push_back(newWidget);   // ajout du pointeur dans la liste
+    }
+
+    void my_Window::delWidget()
+    {
+        widgets.clear();    // nettoyage de la lsite
+        win->clear();       //netoyage de la venetre
     }
 }
--- window.hpp
+++ window.hpp
@@ -10,30 +10,81 @@
 
 namespace Window
 {
+    /// @brief classe principale pour la window
     class my_Window
     {
-        public:
+        public:                                                 // toutes les données suivante seront lisible depuis d'autres fonctions
+            struct Window_info {                                // structure des données nécéssaires a la création idéale de la fenêtre
+                int height;                                     // VARIABLE : hauteur de la fenêtre
+                int lenght;                                     // VARIABLE : largeur de la fenêtre
+                std::string title;                              // VARIABLE : titre
+                int FPS;                                        // VARIABLE : images par secondes
+                sf::Color bg_color;                             // VARIABLE : couleur de l'arrère plan
+            };                                                  // d'autres éléments peuvent être ajouté par la suite
+
+            // ------------------------------------ fontions relative a la window ------------------------------------------------//
+
+            /// @brief destructeur de la window.
             ~my_Window();
-            void window(int h, int l, std::string title);
+
+            /// @brief Création de la window.
+            /// @param data structure de données de
+            /// la window
+            void window(Window_info data);
+
+            /// @brief Boucle principale de la window
             void loop();
+
+            /// @brief get la variable de la window actuelle
+            /// @return variable de la window
+            sf::RenderWindow* get_win() const;
+            // ------------------------------------ fontions relative au widgets ------------------------------------------------//
+
+            /// @brief ajout d'un widget dans une liste avec les infos donnés
+            /// @param data liste de données de widget nécessaire pour le créer
             void addWidget(const WidgetInfo& data);
+
+            /// @brief Suppréssion de tout les widgets de la lsite
             void delWidget();
 
-            void event_handler();
-            void close_win();
-            void resize_win();
-            void key_pressed();
-            void mouse_moved();
-            
-            sf::RenderWindow* get_win() const;
-        private:
-            std::unique_ptr<sf::RenderWindow> win;
-            std::vector<std::shared_ptr<my_widget>> widgets;
-            sf::Event event;
+            // ------------------------------------ fontions relative a la souris ------------------------------------------------//
 
-            sf::Vector2f mouse;
-            sf::Cursor cursor;
+            /// @brief initialisation des données de la souris
+            void set_mouse();
+
+            // ------------------------------------ fontions relative au évenements ----------------------------------------------//
+
+            /// @brief gestion des événements
+            void event_handler();
+
+            /// @brief fermeture de la window
+            void close_win();
+
+            /// @brief changement de la size de la window
+            void resize_win();
+
+            /// @brief gestion des touches préssées
+            void key_pressed();
+
+            /// @brief gestion des événements lorsque la souris est déplacée
+            void mouse_moved();
+
+            /// @brief gestion du déplacement des widget
+            void move_widget();
+        private:
+            std::unique_ptr<sf::RenderWindow> win;              // VARIABLE : valleur de la window
+            std::vector<std::shared_ptr<my_widget>> widgets;    // VARIABLE : liste de vecteurs
+            sf::Event event;                                    // VARIABLE : valeur des evenements
+
+            sf::Vector2f mouse;                                 // VARIABLE : souris
+            sf::Cursor cursor;                                  // VARIABLE : forme de la souris
+
+            bool is_dragging = false;                           // VARIABLE : est-ce qu'on est prêt a déplacer un objet ?
+            my_widget* dragged_widget = nullptr;                // VARIABLE : quel widget on doit déplacer
+            sf::Vector2f drag_offset;                           // VARIABLE : position de l'offset pour le positionnement de la souris sur le widget
     };
 }
 
+/// @brief interface utilisateur
+/// @param win window actuelle
 void UI(Window::my_Window& win);
