2ee68756319306413aa055582dfb59cd4960e480
DarkWhige <clement.duboille@epitech.eu>
2024-06-03 18:09:11 +0200

    Add comments

--- Iwidget.hpp
+++ Iwidget.hpp
@@ -2,9 +2,16 @@
 
 #include <SFML/Graphics.hpp>
 
+/// @brief interface pour les widgets
 class Iwidget {
     public:
+        /// @brief destructeur de la classe
         virtual ~Iwidget() = default;
+
+        /// @brief affichage de l'élément
+        /// @param window variable de la window
         virtual void draw(sf::RenderWindow& window) = 0;
+
+        /// @brief actualisation
         virtual void update() = 0;
 };
